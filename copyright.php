113418f21f733ef36bbb5717a54806f4a3d00a8f
Angelika Specter <angelikaspecter@gmail.com>
2022-05-09 22:14:28 +0300

    UPD php into html

--- copyright.php
+++
@@ -1,8 +0,0 @@
-<div class="copyright">
-	<span>
-		<script>
-			document.write(new Date().getFullYear());
-		</script>
-		© <span data-lang="site_copyright">All rights reserved</span>. <a href="http://t.me/iSpectra" target="_blank"><span data-lang="site_author">Angelika Specter</span></a>
-	</span>
-</div>
